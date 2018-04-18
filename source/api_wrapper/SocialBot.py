5a18cc08a80ac2634e4f656344336a683c2b8d9d
LukFR <dev@lukfr.de>
2018-04-18 17:04:58 +0200

    Updated .gitignore. Restructured the api_wrapper. Created the Logger class. Added funtions to the SocialBot class.

--- source/api_wrapper/SocialBot.py
+++ source/api_wrapper/SocialBot.py
@@ -1,17 +1,16 @@
 import twitter
-import datetime
-from pathlib import Path
+from source.api_wrapper.Logger import Logger
 
 
 class SocialBot:
 
     def __init__(self, access_info):
-
         """
         :param access_info:
 
         Check if access_info contains all necessary information to build an twitter object.
         """
+
         if 'consumer_key' not in access_info:
             raise Exception('Missing consumer_key in access_info')
         elif 'consumer_secret' not in access_info:
@@ -28,94 +27,108 @@ class SocialBot:
             access_token_secret=access_info['access_token_secret']
         )
 
-        self.logging = False
-        self.logFile = ''
         self.overwrite_sensitive = True
+        self.__logger = Logger()
 
     """
-    Activate the action log. Required are a path and a name for the log file.
-    It's sensitive for overwritting files and stops if a speficied file already exists.
+    LOG:
     """
 
     def activate_log(self, path, name):
-        if path == '':
-            raise Exception('Missing path for the logging file')
-        elif name == '':
-            raise Exception('Missing name for logging file')
+        """
+        Activates the logging via the logger instance.
+        :param path:
+        :param name:
+        :return:
+        """
 
-        f = Path(path)
+        self.__logger.activate(path, name, self.overwrite_sensitive)
 
-        if not f.is_dir():
-            raise Exception('The directory "' + path + '" does not exist')
+    def __log(self, message):
+        """
+        Calls the log method of the logger instance.
+        :param message:
+        :return:
+        """
 
-        log_file_path = path + '/' + name + '.txt'
-        f = Path(log_file_path)
-
-        if f.exists() and self.overwrite_sensitive:
-            raise Exception('The logging file already exists')
-
-        self.logging = True
-        self.logFile = path + '/' + name + '.txt'
-
-        file = open(self.logFile, 'a')
-        file.write('Log File for SocialBot created on ' + str(datetime.datetime.now()).split('.')[0] + '\n')
-        file.write('####' + '\n')
-        file.close()
+        self.__logger.log(message)
 
     """
-    Logs a message if a log file was specified with activate_log.
-    It can handle a simple string or a dict as message object.
-    If message is a dict the key and the content are written to the log file.
-    Every log to the log file contains a datestamp.
-    """
-
-    def log(self, message):
-        if self.logging and self.logFile != '':
-            if isinstance(message, str):
-                f = open(self.logFile, 'a')
-                f.write(str(datetime.datetime.now()).split('.')[0] + ' | ' + message + '\n')
-                f.write('####' + '\n')
-                f.close()
-
-            elif isinstance(message, dict):
-                f = open(self.logFile, 'a')
-                f.write(str(datetime.datetime.now()).split('.')[0] + ': ' + '\n')
-                keys = message.keys()
-
-                for key in keys:
-                    f.write(key + ': ' + message[key] + '\n')
-
-                f.write('####' + '\n')
-                f.close()
-
-    """
-    Tweet a text.
+    BASIC ACTIONS:
     """
 
     def tweet(self, text):
+        """
+        Tweet a text.
+        :param text:
+        :return:
+        """
+
         if text == '':
             raise Exception('Missing a tweet text')
 
         resp = self.api.PostUpdate(text)
         print(resp)
 
-        self.log({
+        self.__log({
             'action': 'Tweeted ' + text,
             'response': str(resp)
         })
 
+    def retweet(self, tweet_id):
+        """
+        Retweet a tweet with id.
+        :param tweet_id:
+        :return:
+        """
+
+        if (isinstance(tweet_id, str) and tweet_id != '') or (isinstance(tweet_id, int) and tweet_id <= 0):
+            raise Exception('Missing tweet_id')
+
+        resp = self.api.PostRetweet(tweet_id)
+        print(resp)
+
+        self.__log({
+            'action': 'Retweeted tweet with id "' + tweet_id + '"',
+            'response': str(resp)
+        })
+
     """
-    Search for tweets containing a specified text.
+    SEARCH FUNCTIONS:
     """
 
     def search_tweet(self, text):
+        """
+        Search for tweets containing a specified text.
+        :param text:
+        :return:
+        """
+
         if text == '':
             raise Exception('Missing a search term')
 
         resp = self.api.GetSearch(term=text)
         print(resp)
 
-        self.log({
+        self.__log({
             'action': 'Searched for tweets containing "' + text + '"',
+            'response': str(resp)
+        })
+
+    def search_user(self, username):
+        """
+        Search for a user by username.
+        :param username:
+        :return:
+        """
+
+        if username == '':
+            raise Exception('Missing a username to search')
+
+        resp = self.api.GetUsersSearch(username)
+        print(resp)
+
+        self.__log({
+            'action': 'Searched for user with name "' + username + '"',
             'response': str(resp)
         })
