9ddc285e48b849e9c675f99495d715e5207f773a
Valentin Weber <mail@weberval.de>
2018-05-23 15:23:59 +0200

    added bots dummy

--- source/gui/app.py
+++ source/gui/app.py
@@ -3,5 +3,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def dashboard():
     return render_template('index.html')
+
+@app.route('/bots')
+@app.route('/bots/')
+def bots():
+    return render_template('bots.html')
